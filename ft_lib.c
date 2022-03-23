3c754432e88d1031b6757864a9c018f605b9048b
Vitoria luiza Meyer soares <vmeyer-s@c1r18s15.42lisboa.com>
2022-03-23 00:58:21 +0000

    23/03

---
+++ ft_lib.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lib.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmeyer-s <vmeyer-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 17:57:06 by vmeyer-s          #+#    #+#             */
+/*   Updated: 2022/03/17 17:28:01 by vmeyer-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include "ft_printf.h"
+
+unsigned int	g_c = 0 ;
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *s)
+{
+	if (!s)
+		return (0);
+	while (s[g_c] != '\0')
+		g_c += ft_putchar(s[g_c]);
+	return (g_c);
+}
+
+int	ft_putnbr(int n)
+{
+	unsigned int	nbr;
+
+	g_c = 0;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		nbr = (unsigned int)(n * -1);
+	}
+	else
+		nbr = (unsigned int)n;
+	if (nbr >= 10)
+		ft_putnbr(nbr / 10);
+	g_c += ft_putchar((char)(nbr % 10 + 48));
+	return (g_c);
+}
+
+// int	ft_base(long int n, char *str)
+// {
+// 	unsigned int i;
+
+// 	i = 0;
+// 	while (str[i])
+// 		i++;
+// 	if (n / i != 0)
+// 	{
+// 		ft_base((n % i), str);
+// 		g_c += ;
+
+// 	}
+// }
+
+// int	ft_hexa(unsigned int n)
+// {	
+// }
---
+++ ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmeyer-s <vmeyer-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 20:53:32 by vmeyer-s          #+#    #+#             */
+/*   Updated: 2022/03/17 11:45:58 by vmeyer-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+// counter
+unsigned int	g_c;	
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_base(long int n, char *str);
+int	handle_format(char c, va_list args);
+
+#endif 
